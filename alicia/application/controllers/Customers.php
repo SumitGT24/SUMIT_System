c3886803d0925def59f0697fd8813f9c0021dea1
SumitGT24 <sumitguate@hotmail.com>
2024-09-06 12:59:23 -0600

    Modified add customer form

--- alicia/application/controllers/Customers.php
+++ alicia/application/controllers/Customers.php
@@ -581,7 +581,7 @@ class Customers extends Person_controller
 	{		
 		$return = array(lang('common_first_name'),lang('common_last_name'),lang('common_email'),lang('common_phone_number'),lang('common_address_1'),lang('common_address_2'),lang('common_city'),	lang('common_state'),lang('common_zip'),lang('common_country'),lang('common_comments'),lang('customers_account_number'),lang('common_taxable'),lang('customers_tax_certificate'), lang('customers_company_name'),lang('common_tier_name'));
 		
-		$return[] = lang('common_internal_notes');
+		#$return[] = lang('common_internal_notes');
 
 		for($k=1;$k<=NUMBER_OF_PEOPLE_CUSTOM_FIELDS;$k++)
 		{
