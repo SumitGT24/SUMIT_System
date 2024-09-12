179d19c4b439647f541a576dddb31fb6a1a172c0
SumitGT24 <sumitguate@hotmail.com>
2024-09-12 14:35:57 -0600

    Config Rework #2

--- alicia/application/views/config.php
+++ alicia/application/views/config.php
@@ -50,41 +50,7 @@ $this->load->helper('update');
 	<!--Lista de elementos de configuracion que se guardan-->
 	<div class="row">
 		<?php echo form_open_multipart('config/save/',array('id'=>'config_form','class'=>'form-horizontal', 'autocomplete'=> 'off'));  ?>
-	<?php 
-		$this->load->helper('update');
-		if (is_on_phppos_host() && !is_on_demo_host() && !empty($cloud_customer_info)) {?>
-		<!-- Billing Information -->
-		<div class="col-md-12">
-			<div class="panel panel-piluku">
-				<div class="panel-heading">
-					<?php echo lang("config_billing_info"); ?>
-				</div>
-				<div class="panel-body">
-					<div class="alert alert-info" role="alert"><span class="glyphicon glyphicon-info-sign"></span> <?php echo lang('config_update_billing');?></div>
-					<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_billing')) ?>">	
-						<?php if ($cloud_customer_info['payment_provider'] == 'paypal') { ?>
-							
-							<div class="row">
-								<div class="col-md-10 col-md-offset-2">
-									<?php echo lang('config_billing_is_managed_through_paypal');?>
-								</div>
-							</div>
-							
-						<?php } else { ?>
-							<div class="row">
-								<div class="col-md-4 col-md-offset-2">
-									<a class="btn btn-block btn-update-billing btn-primary" href="https://api.whatsapp.com/send/?phone=50247192667&text&type=phone_number&app_absent=0" target="_blank"><?php echo lang('common_update_billing_info');?></a>				
-								</div>
-								<div class="col-md-4">
-									<a class="btn btn-block btn-update-billing btn-default" href="https://api.whatsapp.com/send/?phone=50247192667&text&type=phone_number&app_absent=0" target="_blank"><?php echo lang('config_cancel_account');?></a>
-								</div>
-							</div>
-						<?php } ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	<?php } ?>		
+	
 		<!--Boton de guardado-->
 	<div class="form-actions">
 		<?php echo form_submit(array(
