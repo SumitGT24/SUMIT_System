c4d5da835eeafea7b24dc668aa7b4a9b5f59395e
SumitGT24 <sumitguate@hotmail.com>
2024-09-13 12:35:23 -0600

    Config rework #29

--- alicia/application/views/config.php
+++ alicia/application/views/config.php
@@ -10,7 +10,7 @@ $this->load->helper('update');
 	 <button class="tablink" onclick="openPage('PaymentMethods', this, 'green')">Formas de pago</button>
 	 <button class="tablink" onclick="openPage('Page2', this, 'blue')">Page 2</button>
 </div>
-
+<?php echo form_open_multipart('config/save/', array('id' => 'config_form', 'class' => 'form-horizontal', 'autocomplete' => 'off')); ?>
 <!-- Contenido de pestañas -->
 <div id="CompanyInfo" class="tabcontent">
 <div class="col-md-12">
@@ -58,7 +58,89 @@ $this->load->helper('update');
 
 <div id="PaymentMethods" class="tabcontent">
   <!-- Aquí se cargará el contenido de formas de pago (page1.php) -->
-  <p>Loading...</p>
+  <div class="col-md-12">
+			<div class="panel panel-piluku">
+				<div class="panel-heading">
+					<?php echo create_section(lang('config_payment_types_info'))  ?>
+				</div>
+				<div class="panel-body">
+						
+					<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_payment')) ?>">	
+						<?php echo form_label(lang('config_payment_types').':', 'additional_payment_types',array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label ')); ?>
+						<div class="col-sm-9 col-md-9 col-lg-10">
+							<a href="#" class="btn btn-primary payment_types"><?php echo lang('common_cash'); ?></a> 
+							<a href="#" class="btn btn-primary payment_types"><?php echo lang('common_check'); ?></a> 
+							<a href="#" class="btn btn-primary payment_types"><?php echo lang('common_giftcard'); ?></a> 
+							<a href="#" class="btn btn-primary payment_types"><?php echo lang('common_debit'); ?></a> 
+							<a href="#" class="btn btn-primary payment_types"><?php echo lang('common_credit'); ?></a>
+							<br>
+							<br>
+							<?php echo form_input(array(
+								'class'=>'form-control form-inps',
+								'name'=>'additional_payment_types',
+								'id'=>'additional_payment_types',
+								'size'=> 40,
+								'value'=>$this->config->item('additional_payment_types')));?>
+						</div>
+					</div>
+					
+					<?php
+					
+					$markup_markdown = array();
+					if ($this->config->item('markup_markdown'))
+					{
+						$markup_markdown = unserialize($this->config->item('markup_markdown'));
+					}
+					
+					foreach(array_keys($this->Sale->get_payment_options_with_language_keys()) as $payment_type)
+					{
+					?>
+						<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_payment')) ?>">
+							<?php echo form_label($payment_type.' '.lang('config_markup_markdown').' '.lang('common_percentage'), 'payment_type_markup_markdown',array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label')); ?>
+							<div class="col-sm-9 col-md-9 col-lg-10">
+								
+								<?php 
+								$markup_down_value = isset($markup_markdown[$payment_type]) ? $markup_markdown[$payment_type] : '';
+								echo form_input(array(
+								'class'=>'form-control form-inps',
+								'name'=>'markup_markdown['.hex_encode($payment_type).']',
+								'id'=>'sale_prefix',
+								'value'=>$markup_down_value));?>
+							</div>
+						</div>
+						
+					<?php
+					}
+					?>
+					
+					<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_payment')) ?>">	
+						<?php echo form_label(lang('config_default_payment_type').':', 'default_payment_type',array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label ')); ?>
+						<div class="col-sm-9 col-md-9 col-lg-10">
+						<?php echo form_dropdown('default_payment_type', $payment_options, $this->config->item('default_payment_type'),'class="form-control" id="default_payment_type"'); ?>
+						</div>
+					</div>
+					
+					<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_payment')) ?>">	
+						<?php echo form_label(lang('config_default_payment_type_recv').':', 'default_payment_type_recv',array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label ')); ?>
+						<div class="col-sm-9 col-md-9 col-lg-10">
+						<?php echo form_dropdown('default_payment_type_recv', $payment_options, $this->config->item('default_payment_type_recv'),'class="form-control" id="default_payment_type_recv"'); ?>
+						</div>
+					</div>
+					
+					<div class="form-group" data-keyword="<?php echo H(lang('config_keyword_payment')) ?>">
+						<?php echo form_label(lang('config_show_selling_price_on_recv'). ':', 'show_selling_price_on_recv',array('class'=>'col-sm-3 col-md-3 col-lg-2 control-label')); ?>
+						<div class="col-sm-9 col-md-9 col-lg-10">
+							<?php echo form_checkbox(array(
+							'name'=>'show_selling_price_on_recv',
+							'id'=>'show_selling_price_on_recv',
+							'value'=>'1',
+							'checked'=>$this->config->item('show_selling_price_on_recv')));?>
+							<label for="show_selling_price_on_recv"><span></span></label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 </div>
 
 <div id="Page2" class="tabcontent">
@@ -68,7 +150,6 @@ $this->load->helper('update');
 
 <!-- Botón de guardado (si es necesario mantenerlo) -->
 <div class="form-actions">
-    <?php echo form_open_multipart('config/save/', array('id' => 'config_form', 'class' => 'form-horizontal', 'autocomplete' => 'off')); ?>
     <!-- Botón de guardado -->
     <?php echo form_submit(array(
         'name' => 'submitf',
