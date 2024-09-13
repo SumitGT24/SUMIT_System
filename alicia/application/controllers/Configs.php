b67668ec735779b9857312673566370ba763db2d
SumitGT24 <sumitguate@hotmail.com>
2024-09-13 08:07:38 -0600

    Config rework #12

---
+++ alicia/application/controllers/Configs.php
@@ -0,0 +1,7 @@
+<?php
+    class Configs extends CI_Controller {
+        public function companyinfo() {
+            $this->load->view('configs/companyinfo');  // Carga la vista
+        }
+    }
+?>
