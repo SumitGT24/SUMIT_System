16c1dc91f1f94ac2091e96214a43ec41e67928a8
SumitGT24 <sumitguate@hotmail.com>
2024-09-16 11:54:33 -0600

    Config rework rework #12 add configs

--- alicia/application/views/config.php
+++ alicia/application/views/config.php
@@ -5,17 +5,18 @@ $this->load->helper('update');
 ?>
 
 <!-- Pestañas -->
-<button class="tablink" onclick="openPage('CompanyInfo', this)" id="defaultOpen">Empresa</button>
-<button class="tablink" onclick="openPage('PaymentMethods', this)">Pagos</button>
-<button class="tablink" onclick="openPage('PriceRules', this)">Reglas de precios</button>
-<button class="tablink" onclick="openPage('Order&Delivery', this)">Ordenes y Entregas</button>
-<button class="tablink" onclick="openPage('Sales', this)">Ventas</button>
-<button class="tablink" onclick="openPage('Receipts', this)">Recibos</button>
-<button class="tablink" onclick="openPage('Profit', this)">Cálculo de ganancias</button>
-<button class="tablink" onclick="openPage('Barcodes', this)">Códigos de barra</button>
-<button class="tablink" onclick="openPage('Employee', this)">Empleados</button>
-<button class="tablink" onclick="openPage('System', this)">Sistema</button>
-
+ <div class="tab'container">
+	 <button class="tablink" onclick="openPage('CompanyInfo', this)" id="defaultOpen">Empresa</button>
+	 <button class="tablink" onclick="openPage('PaymentMethods', this)">Pagos</button>
+	 <button class="tablink" onclick="openPage('PriceRules', this)">Reglas de precios</button>
+	 <button class="tablink" onclick="openPage('Order&Delivery', this)">Ordenes y Entregas</button>
+	 <button class="tablink" onclick="openPage('Sales', this)">Ventas</button>
+	 <button class="tablink" onclick="openPage('Receipts', this)">Recibos</button>
+	 <button class="tablink" onclick="openPage('Profit', this)">Cálculo de ganancias</button>
+	 <button class="tablink" onclick="openPage('Barcodes', this)">Códigos de barra</button>
+	 <button class="tablink" onclick="openPage('Employee', this)">Empleados</button>
+	 <button class="tablink" onclick="openPage('System', this)">Sistema</button>	 
+</div>
 <!-- Formulario de configuraciones -->
 <?php echo form_open_multipart('config/save/', array('id' => 'config_form', 'class' => 'form-horizontal', 'autocomplete' => 'off')); ?>
 	<!-- Informacion de la empresa-->
@@ -56,7 +57,6 @@ $this->load->helper('update');
 						'class'=>'form-control form-inps',
 						'name'=>'website',
 						'id'=>'website',
-						'website'=>$this->input->post('website'),
 						'value'=>$this->config->item('website')));?>
 					</div>
 				</div>
