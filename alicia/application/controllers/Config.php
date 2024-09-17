153ade47b68ffce237b070ba1e1ad95dacf8bc37
SumitGT24 <sumitguate@hotmail.com>
2024-09-17 16:28:49 -0600

    Finally restore config #11

--- alicia/application/controllers/Config.php
+++ alicia/application/controllers/Config.php
@@ -353,7 +353,7 @@ class Config extends Secure_area
 			
 		}
 		
-		$valid_languages = str_replace(DIRECTORY_SEPARATOR,'',directory_map(APPPATH.'language/', 1));
+		#$valid_languages = str_replace(DIRECTORY_SEPARATOR,'',directory_map(APPPATH.'language/', 1));
 		$batch_save_data=array(
 		'company'=>$this->input->post('company'),
 		#'qb_export_start_date'=>$this->input->post('export_start_date'),
@@ -361,7 +361,7 @@ class Config extends Secure_area
 		'website'=>$this->input->post('website'),
 		#'prices_include_tax' => $this->input->post('prices_include_tax') ? 1 : 0,
 		#'currency_symbol'=>$this->input->post('currency_symbol'),
-		'language'=>in_array($this->input->post('language'), $valid_languages) ? $this->input->post('language') : 'english',
+		#'language'=>in_array($this->input->post('language'), $valid_languages) ? $this->input->post('language') : 'english',
 		#'date_format'=>$this->input->post('date_format'),
 		#'time_format'=>$this->input->post('time_format'),
 		'print_after_sale'=>$this->input->post('print_after_sale') ? 1 : 0,
