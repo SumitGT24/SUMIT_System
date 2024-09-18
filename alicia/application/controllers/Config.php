5685832274f5e12e5eae51ad36df18f89af49a40
SumitGT24 <sumitguate@hotmail.com>
2024-09-18 11:53:59 -0600

    Restore save button #2

--- alicia/application/controllers/Config.php
+++ alicia/application/controllers/Config.php
@@ -432,9 +432,9 @@ class Config extends Secure_area
 		'require_employee_login_before_each_sale' => $this->input->post('require_employee_login_before_each_sale') ? 1 : 0,
 		'reset_location_when_switching_employee' => $this->input->post('reset_location_when_switching_employee') ? 1 : 0,
 
-		'number_of_decimals' => $this->input->post('number_of_decimals'),
+		#'number_of_decimals' => $this->input->post('number_of_decimals'),
 		'enhanced_search_method' => $this->input->post('enhanced_search_method') ? 1 : 0,
-		#'commission_percent_type' => $this->input->post('commission_percent_type'),
+		'commission_percent_type' => $this->input->post('commission_percent_type'),
 
 		'hide_price_on_barcodes' => $this->input->post('hide_price_on_barcodes') ? 1 : 0,
 		'always_use_average_cost_method' => $this->input->post('always_use_average_cost_method') ? 1 : 0,
