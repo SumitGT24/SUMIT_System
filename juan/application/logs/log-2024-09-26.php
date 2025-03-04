0e6a397b626a72c19d727b7cd809ac858d21ad97
SumitGT24 <sumitguate@hotmail.com>
2025-03-04 15:24:54 -0600

    ✅ Usuario juan añadido

---
+++ juan/application/logs/log-2024-09-26.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-09-26 18:16:36 --> Severity: error --> Exception: Access denied for user 'root'@'190.104.121.45' (using password: YES) /home/sumit/Documentos/Sumit_Repo/public_html/alicia/system/database/drivers/mysqli/mysqli_driver.php 203
