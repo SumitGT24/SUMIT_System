9b2aced27b6d68fbccfa7566600f70030be2c580
SumitGT24 <sumitguate@hotmail.com>
2025-03-04 16:33:19 -0600

    ✅ Usuario pedro añadido

---
+++ pedro/application/language/spanish/appointments_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['appointments_confirm_delete'] = '¿Seguro que quieres borrar esta VISITA TECNICA?';
+$lang['appointments_none_selected'] = 'No tienes ninguna VISITA seleccionada.';
+$lang['appointments_calendar'] = 'Calendario';
+$lang['appointments_manage_deleted'] = 'Administrar VISITAS eliminadas';
+$lang['appointments_appointment_date_start'] = 'Fecha de inicio de la VISITA';
+$lang['appointments_appointment_date_end'] = 'Fecha de finalización de la VISITA';
+$lang['appointments_new'] = 'NUEVA VISITA TECNICA';
+$lang['appointments_start_date'] = 'Inicio de VISITA';
+$lang['appointments_end_date'] = 'Cita finalizada';
+$lang['appointments_no_appointments_to_display'] = 'No hay citas para mostrar';
+$lang['appointments_update'] = 'Actualizar';
+$lang['appointments_successful_deleted'] = 'Citas eliminadas con éxito';
+$lang['appointments_cannot_be_deleted'] = 'No se pueden borrar citas';
+$lang['appointments_one_or_multiple'] = 'Equipo)';
+$lang['appointments_confirm_undelete'] = '¿Está seguro de que desea recuperar las citas seleccionadas?';
+$lang['appointments_successful_undeleted'] = 'Tienes citas recuperadas exitosamente';
+$lang['appointments_cannot_be_undeleted'] = 'Las citas no pueden ser recuperadas';
+$lang['appointments_select_date'] = 'Seleccione fecha';
+$lang['appointments_manage_appointment_types'] = 'Administrar categorías de citas';
+$lang['appointments_todays_appointments'] = 'VISITAS de hoy';
+$lang['appointments_appointment_person'] = '';
+?>
