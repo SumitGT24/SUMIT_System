d5c7d27f7c855af161e4a51ad03d9de703fcd6c1
SumitGT24 <sumitguate@hotmail.com>
2025-03-13 16:27:14 -0600

    ✅ Usuario demo añadido

---
+++ demo/application/language/spanish/home_lang.php
@@ -0,0 +1,34 @@
+<?php
+$lang['home_todays_closeout_report'] = 'INFORME DEL CIERRE DE HOY';
+$lang['home_todays_detailed_sales_report'] = 'DETALLES DEL CIERRE DE HOY';
+$lang['home_todays_summary_items_report'] = 'REPORTE RESUMIDO DE ARTÍCULOS DEL CIERRE DE HOY';
+
+$lang['home_receivings_start_new_receiving'] = 'REGISTRAR NUEVAS COMPRAS CON FACTURAS';
+$lang['home_mercury_activate_promo_text'] = 'El nuevo modulo de reparaciones permite llevar un mejor control de los servicios de reparacion y/o mantenimiento que se realicen, llevando un control especifico del seguimiento de cada uno de los casos.';
+$lang['home_welcome_message'] = '¡SUMIT TE DA LA BIENVENIDA, seleccione una opción para comenzar!';
+$lang['home_ecommerce_platform_sync'] = 'Estado de la sincronización plataforma de Comercio electrónico';
+$lang['home_view_saved_reports'] = 'VER REPORTES GUARDADOS';
+
+$lang['home_quickbooks_platform_sync'] = 'Estado de sincronización de Quickbooks';
+
+$lang['home_wizard_configure_company'] = 'Configure la información de su empresa';
+$lang['home_wizard_configure_locations'] = 'Configure sus ubicaciones';
+$lang['home_wizard_add_inventory'] = 'Añada su inventario / artículos';
+$lang['home_wizard_add_customer'] = 'Añade tus clientes';
+$lang['home_wizard_create_sale'] = 'Completa tu primera venta';
+$lang['home_setup_wizard'] = 'Complete los pasos para mejorar la experiencia del uso del software ';
+
+
+$lang['home_wizard_edit_employees'] = 'Agregar y actualizar empleados';
+
+$lang['home_items_expiring_soon'] = 'Artículos que caducan pronto';
+$lang['home_reseller_program_signup'] = '¡Gane un 20% por cada cliente que adquiera el software punto de venta y control de inventarios!';
+$lang['home_signup_now'] = 'Escribanos ahora!';
+$lang['home_resellers_program'] = 'Programa de revendedor';
+$lang['home_bluejay'] = 'El software esta desarrollado en lenguaje de programación php y base de datos mysql, por tal razón es un software ambiente web, su diseño responsivo 
+hace que sea compatible con la mayoria de dispositivos moviles y este disponible en todo momento y en cualquier lugar sin necesidad de instalar ninguna aplicacion, basta con abrir su navegador... Para mas información sobre otros beneficios puede escribirnos...';
+$lang['home_bluejay_reviews'] = 'Software en todo Momento:';
+$lang['home_feedback_program'] = 'Al adquir el software usted también adquiere la ayuda y soporte para instalación, implementación, asi como la ayuda para echar a andar el Software...';
+$lang['home_feedback'] = 'Soporte y Respaldo';
+$lang['home_visit_now'] = 'Visitar ahora';
+?>
