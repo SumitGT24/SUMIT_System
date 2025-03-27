b44ab6168a746bafdf253332d72b1906b15116d7
SumitGT24 <sumitguate@hotmail.com>
2025-03-27 12:27:44 -0600

    ✅ Usuario sumit añadido

---
+++ sumit/application/logs/log-2025-03-19.php
@@ -0,0 +1,109 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-19 11:05:13 --> Severity: error --> Exception: Property emailDTE does not exist /opt/lampp/htdocs/public_html/alicia/application/models/cart/PHPPOSCart.php 175
+ERROR - 2025-03-19 11:13:21 --> Severity: Warning --> Array to string conversion /opt/lampp/htdocs/public_html/alicia/system/helpers/form_helper.php 972
+ERROR - 2025-03-19 11:15:27 --> Severity: Warning --> file_put_contents(facturas/685EDABE-6717-4B25-B2C8-15770A8D25ED.xml): Failed to open stream: No existe el archivo o el directorio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2361
+ERROR - 2025-03-19 11:26:34 --> Severity: error --> Exception: Property correoDTE does not exist /opt/lampp/htdocs/public_html/alicia/application/models/cart/PHPPOSCart.php 175
+ERROR - 2025-03-19 12:00:39 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/alicia/application/views/home.php 455
+ERROR - 2025-03-19 12:17:24 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/alicia/application/views/home.php 455
+ERROR - 2025-03-19 12:17:31 --> Could not find the language line "customers_add_new_customer"
+ERROR - 2025-03-19 12:17:34 --> Could not find the language line "customers_add_new_customer"
+ERROR - 2025-03-19 12:17:40 --> Could not find the language line "customers_add_new_customer"
+ERROR - 2025-03-19 12:18:29 --> Could not find the language line "customers_add_new_customer"
+ERROR - 2025-03-19 12:43:14 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 12:43:14 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:01:08 --> Severity: Warning --> Array to string conversion /opt/lampp/htdocs/public_html/alicia/system/helpers/form_helper.php 972
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:11:32 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:11:33 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:11:33 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:11:33 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:11:33 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:12:04 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:12:05 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:12:23 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:12:24 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:18:38 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:18:39 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:18:39 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:18:39 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:18:39 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:20:09 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:40:38 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/alicia/application/views/home.php 455
+ERROR - 2025-03-19 15:43:53 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/alicia/application/views/home.php 455
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:51:12 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:51:13 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:51:13 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:51:13 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:51:13 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$tipo_contribuyente /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2135
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$razon_social /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2268
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$number_establecimiento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2269
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$municipio /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2274
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$departamento /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2275
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$mensaje_fact /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2304
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2310
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2323
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$xmlSigned /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2331
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$nit /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2336
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$api_key /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2350
+ERROR - 2025-03-19 15:56:29 --> Severity: Warning --> Undefined property: stdClass::$uuid /opt/lampp/htdocs/public_html/alicia/application/controllers/Sales.php 2360
