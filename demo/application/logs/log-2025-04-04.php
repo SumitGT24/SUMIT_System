48ba05046f9c87155eb896b8d4ec9e03a19663cc
SumitGT24 <sumitguate@hotmail.com>
2025-04-07 16:10:31 -0600

    ⬆️ Usuario demo actualizado

--- demo/application/logs/log-2025-04-04.php
+++ demo/application/logs/log-2025-04-04.php
@@ -11,3 +11,5 @@ ERROR - 2025-04-04 16:24:27 --> Could not find the language line "customers_merg
 ERROR - 2025-04-04 16:24:28 --> Could not find the language line "reports_appointments"
 ERROR - 2025-04-04 16:24:37 --> Severity: Warning --> Undefined property: stdClass::$mailchimp_api_key /opt/lampp/htdocs/public_html/demo/application/models/Location.php 143
 ERROR - 2025-04-04 16:24:50 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/demo/application/views/home.php 455
+ERROR - 2025-04-04 16:39:56 --> Severity: Warning --> Undefined variable $total_deliveries /opt/lampp/htdocs/public_html/demo/application/views/home.php 455
+ERROR - 2025-04-04 16:40:01 --> Could not find the language line "locationsadmin_confirm_delete"
